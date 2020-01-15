5e4a3fb5220517209fe813dd0189a6ec26116a62
tckz <at.tckz@gmail.com>
2020-01-15 13:07:44 +0900

    HTTPリクエストが500応答のときにerr==nilであることの確認

---
+++ http-res-err.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"net/url"
+)
+
+func main() {
+
+	m := &http.ServeMux{}
+	m.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "hello\n")
+	})
+
+	testServer := httptest.NewServer(m)
+	defer testServer.Close()
+
+	vals := url.Values{}
+	vals.Add("key", "param")
+
+	// 500応答でもerrではない、という確認
+	res, err := http.PostForm(testServer.URL, vals)
+	if err != nil {
+		log.Printf("err=%v\n", err)
+		return
+	}
+
+	defer res.Body.Close()
+
+	dump, err := httputil.DumpResponse(res, true)
+	if err != nil {
+		log.Fatalf("*** httputil.DumpResponse: %v\n", err)
+	}
+
+	fmt.Println(string(dump))
+}
