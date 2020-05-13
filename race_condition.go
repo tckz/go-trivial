7b77b521b5cfca90bccaf2468bd79adc970ff938
tckz <at.tckz@gmail.com>
2020-05-13 21:36:41 +0900

    race_condition

--- race_condition.go
+++ race_condition.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 // -race付きで実行するとrace detectorが検知する
 // GORACE="halt_on_error=1"
 
 func main() {
-	num := 10
+	num := 0
+	ch := make(chan int)
 	go func() {
-		num += 1
-		fmt.Println(num)
+		for i := 0; i < 100000; i++ {
+			num += 1
+		}
+		fmt.Printf("other: %d\n", num)
+		ch <- 1
 	}()
-	num += 1
-	fmt.Println(num)
-	time.Sleep(time.Second)
+	for i := 0; i < 100000; i++ {
+		num += 1
+	}
+	fmt.Printf("main: %d\n", num)
+	<-ch
 }
