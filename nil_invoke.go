b8b91d237d81cc106a95168b930ced173489de54
tckz <at.tckz@gmail.com>
2020-05-20 17:34:29 +0900

    nil_invoke: レシーバーがnilでもmethod呼び出せる。

---
+++ nil_invoke.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type some struct {
+	field int
+}
+
+func (s *some) someFunc() {
+	// レシーバーがnilでも普通に呼び出せる
+	fmt.Fprintf(os.Stderr, "someFunc: %v\n", s)
+}
+
+func main() {
+
+	s1 := &some{}
+	s1.someFunc()
+	fmt.Fprintf(os.Stderr, "s1: %v\n", s1.field)
+
+	var s2 *some
+	s2.someFunc()
+	// nilに対してフィールドを参照するのはpanic
+	fmt.Fprintf(os.Stderr, "s1: %v\n", s2.field)
+}
