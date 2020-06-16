8e86f04d33b47ead998244068f496f2343b49ea8
tckz <at.tckz@gmail.com>
2020-06-16 14:48:39 +0900

    slice-part: 部分スライス

---
+++ slice-part.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func r(f func() interface{}) (ret interface{}) {
+	defer func() {
+		if r := recover(); r != nil {
+			ret = r
+		}
+	}()
+
+	return f()
+}
+
+func main() {
+	a := []int{11, 12, 13, 14, 15}
+
+	// [:1]=[11]
+	fmt.Printf("[:1]=%v\n", r(func() interface{} { return a[:1] }))
+	// [:]=[11 12 13 14 15]
+	fmt.Printf("[:5]=%v\n", r(func() interface{} { return a[:5] }))
+	// [:6]=runtime error: slice bounds out of range [:6] with capacity 5
+	fmt.Printf("[:6]=%v\n", r(func() interface{} { return a[:6] }))
+	// [:]=[11 12 13 14 15]
+	fmt.Printf("[:]=%v\n", r(func() interface{} { return a[:] }))
+	// [0:2]=[11 12]
+	fmt.Printf("[0:2]=%v\n", r(func() interface{} { return a[0:2] }))
+	// [3:3]=[]
+	fmt.Printf("[3:3]=%v\n", r(func() interface{} { return a[3:3] }))
+	// [3:2]=runtime error: slice bounds out of range [3:2]
+	// To avoid compile error
+	i := 2
+	fmt.Printf("[3:2]=%v\n", r(func() interface{} { return a[3:i] }))
+}
