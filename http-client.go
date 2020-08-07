04d18a13e0611b645dfc2d0a54d26caf839a52fb
tckz <at.tckz@gmail.com>
2020-08-07 15:33:46 +0900

    http-client: HTTP/2のログなどを見るためにGETを投げるもの。
     o GODEBUG=http2debug=2
     o GODEBUG=http2debug=2,http2client=0

---
+++ http-client.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+	"net/http/httputil"
+	"os"
+)
+
+var (
+	optURL    = flag.String("url", "https://www.yahoo.co.jp", "")
+	optBearer = flag.String("bearer", "", "")
+)
+
+// GODEBUG=http2debug=2
+//  -> HTTP/2でやり取りすればstderrに出てくる
+// GODEBUG=http2debug=2,http2client=0
+//  -> http2クライアント無効
+
+func main() {
+	flag.Parse()
+
+	req, err := http.NewRequest("GET", *optURL, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	if *optBearer != "" {
+		req.Header.Set("Authorization", "Bearer "+*optBearer)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	b, err := httputil.DumpRequest(req, true)
+	if err != nil {
+		panic(err)
+	}
+
+	os.Stdout.Write(b)
+	os.Stdout.WriteString("\n")
+
+	b, err = httputil.DumpResponse(res, true)
+	if err != nil {
+		panic(err)
+	}
+
+	os.Stdout.Write(b)
+	os.Stdout.WriteString("\n")
+}
