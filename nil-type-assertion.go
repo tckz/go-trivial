258bba273d0dec8fe937f5a1c690f916de36cab0
tckz <at.tckz@gmail.com>
2020-08-21 11:24:25 +0900

    nil-type-assertion: 型があわないtype assertionでもokを受けとればpanicはしない

--- nil-type-assertion.go
+++ nil-type-assertion.go
@@ -19,6 +19,17 @@ func main() {
 	v := v1().(*someType)
 	fmt.Fprintf(os.Stderr, "v=%v\n", v)
 
+	// 型が合わなくてもokを受ければpanicせず判定できる
+	func() {
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Fprintf(os.Stderr, "panic=%v\n%s", r, string(debug.Stack()))
+			}
+		}()
+		v, ok := v1().(*someType2)
+		fmt.Fprintf(os.Stderr, "v=%v, ok=%t\n", v, ok)
+	}()
+
 	// 型が合わないとpanic
 	func() {
 		defer func() {
