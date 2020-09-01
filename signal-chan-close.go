c9b6598e7b74bf3f635e20588c032cd99609d264
tckz <at.tckz@gmail.com>
2020-09-01 14:14:15 +0900

    signal-chan-close: signal.Notifyに使っているchanをcloseしてしまうとsignalを受けたときにchanに書き込んでpanicする

---
+++ signal-chan-close.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+// signal.Notifyに使っているchanをcloseしてしまうとsignalを受けたときにchanに書き込んでpanicする
+
+func main() {
+	sig := make(chan os.Signal, 10)
+	signal.Notify(sig, syscall.SIGINT)
+
+	close(sig)
+	// sleepの間にCTRL+Cを押す
+	log.Printf("Waiting SIGINT for 10 seconds ...\n")
+	// ここで待ちを入れてsignalハンドラーが処理をする余地を持たせる。いきなりmainを抜けるとハンドラーがchanに書き込む前にプロセス終了してしまう
+	time.Sleep(10 * time.Second)
+	// chanがcloseしているので panic: send on closed channel になる
+}
