ad5e7e26cbbb9d3478d67f2178ea9e6125da7541
tckz <at.tckz@gmail.com>
2020-11-12 10:42:07 +0900

    nil_map.go: comment

--- nil_map.go
+++ nil_map.go
@@ -14,14 +14,18 @@ type Some struct {
 
 func main() {
 	s := &Some{}
+	// nil?=true, v=false
 	fmt.Fprintf(os.Stderr, "nil?=%v, v=%v\n", s.MapBool == nil, s.MapBool["who?"])
+	// nil?=true, v=
 	fmt.Fprintf(os.Stderr, "nil?=%v, v=%v\n", s.MapString == nil, s.MapString["wao!"])
 
 	// mapのmap
 	m := map[string]map[string]string{
 		"key": map[string]string{},
 	}
+	// empty?=true, v=
 	fmt.Fprintf(os.Stderr, "empty?=%v, v=%v\n", m["key"]["not_exist"] == "", m["key"]["not_exist"])
 	// 最初の階層で存在しないキーを参照してさらにその配下を参照->普通に参照できる。指定キーが存在しない扱いのようだ
+	// empty?=true, v=
 	fmt.Fprintf(os.Stderr, "empty?=%v, v=%v\n", m["not_exist"]["not_exist2"] == "", m["not_exist"]["not_exist2"])
 }
