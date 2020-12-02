2376d88a7ed83de7d10af8e129bb11192a837cca
tckz <at.tckz@gmail.com>
2020-12-02 10:51:09 +0900

    slice-part: text表現、[:]が別sliceになるか->ならない

--- slice-part.go
+++ slice-part.go
@@ -15,22 +15,34 @@ func r(f func() interface{}) (ret interface{}) {
 func main() {
 	a := []int{11, 12, 13, 14, 15}
 
-	// [:1]=[11]
-	fmt.Printf("[:1]=%v\n", r(func() interface{} { return a[:1] }))
-	// [:]=[11 12 13 14 15]
-	fmt.Printf("[:5]=%v\n", r(func() interface{} { return a[:5] }))
-	// [:6]=runtime error: slice bounds out of range [:6] with capacity 5
-	fmt.Printf("[:6]=%v\n", r(func() interface{} { return a[:6] }))
-	// [6:]=runtime error: slice bounds out of range [6:5]
-	fmt.Printf("[6:]=%v\n", r(func() interface{} { return a[6:] }))
-	// [:]=[11 12 13 14 15]
-	fmt.Printf("[:]=%v\n", r(func() interface{} { return a[:] }))
-	// [0:2]=[11 12]
-	fmt.Printf("[0:2]=%v\n", r(func() interface{} { return a[0:2] }))
-	// [3:3]=[]
-	fmt.Printf("[3:3]=%v\n", r(func() interface{} { return a[3:3] }))
-	// [3:2]=runtime error: slice bounds out of range [3:2]
+	var anil []int
+
+	// a=[11 12 13 14 15]
+	fmt.Printf("a=%v\n", r(func() interface{} { return a }))
+	// a[:1]=[11]
+	fmt.Printf("a[:1]=%v\n", r(func() interface{} { return a[:1] }))
+	// a[:]=[11 12 13 14 15]
+	fmt.Printf("a[:5]=%v\n", r(func() interface{} { return a[:5] }))
+	// a[:6]=runtime error: slice bounds out of range [:6] with capacity 5
+	fmt.Printf("a[:6]=%v\n", r(func() interface{} { return a[:6] }))
+	// a[6:]=runtime error: slice bounds out of range [6:5]
+	fmt.Printf("a[6:]=%v\n", r(func() interface{} { return a[6:] }))
+	// a[:]=[11 12 13 14 15]
+	fmt.Printf("a[:]=%v\n", r(func() interface{} { return a[:] }))
+	// a[:]とした場合に別のsliceになるかどうか -> 同じ
+	// a=0xc000122060, [:]=0xc000122060
+	fmt.Printf("a=%p, a[:]=%p\n", a, a[:])
+	// nilにappendして無理やり別のsliceに
+	// a=0xc000122060, append(nil, a...)=0xc0001220c0
+	fmt.Printf("a=%p, append(nil, a...)=%p\n", a, append(anil, a...))
+	// a[0:2]=[11 12]
+	fmt.Printf("a[0:2]=%v\n", r(func() interface{} { return a[0:2] }))
+	// a[1:2]=[12]
+	fmt.Printf("a[1:2]=%v\n", r(func() interface{} { return a[1:2] }))
+	// a[3:3]=[]
+	fmt.Printf("a[3:3]=%v\n", r(func() interface{} { return a[3:3] }))
+	// a[3:2]=runtime error: slice bounds out of range [3:2]
 	// To avoid compile error
 	i := 2
-	fmt.Printf("[3:2]=%v\n", r(func() interface{} { return a[3:i] }))
+	fmt.Printf("a[3:2]=%v\n", r(func() interface{} { return a[3:i] }))
 }
