f40c769ceeced49a102c5bcdcc996906a0f0f4ac
tckz <at.tckz@gmail.com>
2020-12-24 13:18:36 +0900

    改名と、time.TimeをstringからUnmarshal例

--- json-marshal-time.go
+++ json-marshal-time.go
@@ -28,4 +28,9 @@ func main() {
 	}
 	// Marshal={"Name":"myname","SomeTime":"2020-09-11T13:16:28.988983366+09:00"}
 	fmt.Fprintf(os.Stderr, "Marshal=%s\n", string(b))
+
+	var m2 MyType
+	json.Unmarshal(b, &m2)
+	// Unmarshal={Name:myname SomeTime:2020-12-24 13:16:43.136699001 +0900 JST}
+	fmt.Fprintf(os.Stderr, "Unmarshal=%+v\n", m2)
 }
