a404f0b4ce775700a415b811b7787f934d053560
tckz <at.tckz@gmail.com>
2021-01-26 13:07:49 +0900

    context/without_cancel

--- context/without_cancel.go
+++ context/without_cancel.go
@@ -32,3 +32,16 @@ func WithoutCancel(ctx context.Context) context.Context {
 		Context: ctx,
 	}
 }
+
+// WithoutCancelInheritDeadline キャンセルチェーンを切り離しつつ、deadlineがあれば引き継ぐ
+// 例えば、クライアントの接続断からcancelされる状況で接続断されても処理継続したいが、
+// Lambdaのように処理時間が決められていて上流からdeadlineを指定されている場合は従いたい、という状況。
+func WithoutCancelInheritDeadline(ctx context.Context) (ret context.Context, cancel context.CancelFunc) {
+	ret = WithoutCancel(ctx)
+	if d, ok := ctx.Deadline(); ok {
+		return context.WithDeadline(ret, d)
+	} else {
+		cancel = func() {}
+	}
+	return
+}
--- context/without_cancel_test.go
+++ context/without_cancel_test.go
@@ -29,8 +29,10 @@ func TestNoCancel(t *testing.T) {
 		canceled = true
 	default:
 	}
+	// 親のctxがキャンセルされていることを確認
 	assert.True(canceled)
 
+	// 値は最初のctxも含めてつながる
 	assert.Equal("val", ctxWithoutCancel.Value(dummyKey("key")))
 	assert.Equal("val2", ctxWithoutCancel.Value(dummyKey("key2")))
 	canceled2 := false
@@ -39,6 +41,7 @@ func TestNoCancel(t *testing.T) {
 		canceled2 = true
 	default:
 	}
+	// 親がキャンセルされても自分はキャンセルされてない
 	assert.False(canceled2)
 
 	// go1.13からContext.String()の出力が変わった
@@ -49,4 +52,123 @@ func TestNoCancel(t *testing.T) {
 	assert.Equal(time.Time{}, dead)
 
 	assert.Nil(ctxWithoutCancel.Err(), "no error is set")
+}
+
+func TestNoCancelInheritDeadline(t *testing.T) {
+	assert := assert.New(t)
+
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, dummyKey("key"), "val")
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	dead := time.Now().UTC().Add(time.Millisecond * 200)
+	ctx, cancelDeadline := context.WithDeadline(ctx, dead)
+	defer cancelDeadline()
+
+	ctxWithoutCancel, cancelInheritDeadline := WithoutCancelInheritDeadline(ctx)
+	defer cancelInheritDeadline()
+	ctxWithoutCancel = context.WithValue(ctxWithoutCancel, dummyKey("key2"), "val2")
+
+	cancel()
+	canceled := false
+	select {
+	case <-ctx.Done():
+		canceled = true
+	default:
+	}
+	// 親のctxがキャンセルされていることを確認
+	assert.True(canceled)
+
+	// 値は最初のctxも含めてつながる
+	assert.Equal("val", ctxWithoutCancel.Value(dummyKey("key")))
+	assert.Equal("val2", ctxWithoutCancel.Value(dummyKey("key2")))
+	canceled2 := false
+	select {
+	case <-ctxWithoutCancel.Done():
+		canceled2 = true
+	default:
+	}
+	// 親がキャンセルされても自分はキャンセルされてない
+	assert.False(canceled2)
+
+	d, ok := ctxWithoutCancel.Deadline()
+	assert.True(ok, "deadline is set")
+	assert.Equal(dead, d)
+	assert.Nil(ctxWithoutCancel.Err(), "no error is set")
+
+	canceled3 := false
+	select {
+	case <-time.After(time.Millisecond * 500):
+	case <-ctxWithoutCancel.Done():
+		canceled3 = true
+	}
+	// deadline経過でキャンセルされる
+	assert.True(canceled3, "should deadline exceeded")
+	assert.EqualError(ctxWithoutCancel.Err(), "context deadline exceeded")
+}
+
+func TestNoCancelNoInheritDeadline(t *testing.T) {
+	assert := assert.New(t)
+
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, dummyKey("key"), "val")
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	// 親のctxがdeadlineを持っていない場合
+
+	ctxWithoutCancel, cancelInheritDeadline := WithoutCancelInheritDeadline(ctx)
+	defer cancelInheritDeadline()
+	ctxWithoutCancel = context.WithValue(ctxWithoutCancel, dummyKey("key2"), "val2")
+
+	// 親のctxをキャンセルしキャンセルされていることを確認
+	cancel()
+	{
+		canceled := false
+		select {
+		case <-ctx.Done():
+			canceled = true
+		default:
+		}
+		assert.True(canceled)
+	}
+
+	// 値は最初のctxも含めてつながる
+	assert.Equal("val", ctxWithoutCancel.Value(dummyKey("key")))
+	assert.Equal("val2", ctxWithoutCancel.Value(dummyKey("key2")))
+	// 親がキャンセルされても自分はキャンセルされてない
+	{
+		canceled := false
+		select {
+		case <-ctxWithoutCancel.Done():
+			canceled = true
+		default:
+		}
+		assert.False(canceled)
+	}
+
+	d, ok := ctxWithoutCancel.Deadline()
+	assert.False(ok, "no deadline is set")
+	assert.Equal(time.Time{}, d)
+
+	assert.Nil(ctxWithoutCancel.Err(), "no error is set")
+
+	timeout := false
+	select {
+	case <-time.After(time.Millisecond * 200):
+		timeout = true
+	case <-ctxWithoutCancel.Done():
+	}
+	assert.True(timeout, "should be timed out")
+	// timeout経過で抜けてきて、キャンセルはされていない
+	{
+		canceled := false
+		select {
+		case <-ctxWithoutCancel.Done():
+			canceled = true
+		default:
+		}
+		assert.False(canceled)
+	}
 }
