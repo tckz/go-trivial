17ceab6aef981dbccadbaebe588b5d0df0f325b2
tckz <at.tckz@gmail.com>
2021-04-12 10:39:21 +0900

    fmt-percent-w.go: 比較で%s,%vなども

--- fmt-percent-w.go
+++ fmt-percent-w.go
@@ -10,6 +10,14 @@ func main() {
 	// %wをfmt.Errorf以外で使ってるコードがあったので確認
 	err := errors.New("error desu")
 
-	// err=%!w(*errors.errorString=&{error desu}) dayo
-	fmt.Fprintf(os.Stderr, "err=%w dayo\n", err)
+	// 1:err=%!w(*errors.errorString=&{error desu}) dayo
+	fmt.Fprintf(os.Stderr, "1:err=%w dayo\n", err)
+	// 2:err=error desu dayo
+	fmt.Fprintf(os.Stderr, "2:err=%v dayo\n", err)
+	// 3:err=&errors.errorString{s:"error desu"} dayo
+	fmt.Fprintf(os.Stderr, "3:err=%#v dayo\n", err)
+	//4:err=error desu dayo
+	fmt.Fprintf(os.Stderr, "4:err=%+v dayo\n", err)
+	// 5:err=error desu dayo
+	fmt.Fprintf(os.Stderr, "5:err=%s dayo\n", err)
 }
