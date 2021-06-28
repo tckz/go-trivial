5cc42b6f88ac60d3e1585ab11f4a89933cb7325d
tckz <at.tckz@gmail.com>
2021-06-28 13:42:45 +0900

    alloc/alloc_test: comment

--- alloc/alloc_test.go
+++ alloc/alloc_test.go
@@ -6,6 +6,16 @@ import (
 
 // スライスをポインターで返して、デリファレンスするとallocateがある、という確認
 // go test -bench . -benchmem
+/*
+go1.15.12 test -bench . -benchmem
+Benchmark_Pointer-8         5859            213174 ns/op          826007 B/op         21 allocs/op
+Benchmark_Slice-8           7021            177595 ns/op          825973 B/op         20 allocs/op
+*/
+/*
+go1.16だとallocateされない
+Benchmark_Pointer-8         6999            174979 ns/op          825975 B/op         20 allocs/op
+Benchmark_Slice-8           5743            181383 ns/op          825976 B/op         20 allocs/op
+*/
 
 func createSlice1() *[]string {
 	var s []string
