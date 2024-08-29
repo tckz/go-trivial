7ac8f4113e22bdd9e5b087944a36786b8ead3b97
tckz <at.tckz@gmail.com>
2024-08-29 13:12:56 +0900

    go-redis: .Int()

---
+++ go-redis/go-redis-get-none-int.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/alicebob/miniredis/v2"
+	"github.com/go-redis/redis/v8"
+)
+
+// goredisのGETでキーが存在しないInt()の挙動
+
+func main() {
+
+	mr, err := miniredis.Run()
+	if err != nil {
+		panic(err)
+	}
+	defer mr.Close()
+	addr := mr.Addr()
+
+	cl := redis.NewUniversalClient(&redis.UniversalOptions{
+		Addrs: []string{addr},
+	})
+	defer cl.Close()
+
+	// キー自体がない -> "", redis.Nil
+	ret, err := cl.Get(context.Background(), "not_exist").Int()
+	// ret=0, err=redis: nil, redis.Nil?=true
+	log.Printf("ret=%v, err=%v, redis.Nil?=%v", ret, err, err == redis.Nil)
+}
