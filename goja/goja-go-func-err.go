4cc8f0c20d76d9e88cd4be79d3d00648f830a860
tckz <at.tckz@gmail.com>
2024-11-11 20:21:21 +0900

    goja-go-func-err: goの間数をjsから呼び出し、そのgo間数がerrorを返した場合にjsから飛び出た例外をgo側で見ると元のerrorを取り出せる

---
+++ goja/goja-go-func-err.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/dop251/goja"
+	"github.com/dop251/goja/parser"
+	"github.com/dop251/goja_nodejs/console"
+	"github.com/dop251/goja_nodejs/require"
+	"github.com/tckz/go-trivial/goja/common"
+)
+
+type ExampleError struct {
+	s string
+}
+
+func (e *ExampleError) Error() string {
+	return e.s
+}
+
+func main() {
+	vm := goja.New()
+
+	// https://github.com/dop251/goja/issues/116
+	require.NewRegistry().Enable(vm)
+	vm.SetParserOptions(parser.WithDisableSourceMaps)
+	console.Enable(vm)
+
+	// goの関数を渡してjs側で呼び出しそれがエラーになったときどんな例外になるか
+	// goja.GoErrorでwrapされ、元のgoのerrorとして取り出せる
+
+	common.MustSet(vm, "goFunc", func(v int) error {
+		return &ExampleError{s: fmt.Sprintf("goFunc error: %v", v)}
+	})
+	v, err := vm.RunString(`
+goFunc(123);
+`)
+	fmt.Fprintf(os.Stderr, "value(%T)=%s, err(%T)=%+v\n", v, v, err, common.GojaErrorString(err))
+
+	var ee *ExampleError
+	ok := errors.As(err, &ee)
+	// ok=true, ee=goFunc error: 123
+	fmt.Fprintf(os.Stderr, "ok=%t, ee=%+v\n", ok, ee)
+}
