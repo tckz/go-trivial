db1bf9869ab625d2004ed972c46dfdda559b6643
tckz <at.tckz@gmail.com>
2026-03-05 21:58:09 +0900

    fmt-percent-d-slice

--- stdlib/fmt/fmt-percent-d-slice.go
+++ stdlib/fmt/fmt-percent-d-slice.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	// %dにスライスを渡すと%vと同じようにスライスっぽい表現になる
+	// %dにintスライスを渡すと%vと同じようにスライスっぽい表現になる
 
 	// []
 	var s []int
@@ -15,4 +15,8 @@ func main() {
 	// [1 2 3]
 	s2 := []int{1, 2, 3}
 	fmt.Fprintf(os.Stderr, "%d\n", s2)
+
+	// [%!d(string=a) %!d(string=b) %!d(string=c)]
+	s3 := []string{"a", "b", "c"}
+	fmt.Fprintf(os.Stderr, "%d\n", s3)
 }
